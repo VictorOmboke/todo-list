255d1cff6e0f50609e02f8e08acd1f6a5aea91b4
Victor Omboke <victoromboke@gmail.com>
2024-02-11 19:31:12 -0800

    Clear working tree

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import handleDomManipulation from "./dom-manipulation.js";
+import handleTodoLogic from "./todo-logic.js";
+
+const domManipulation = handleDomManipulation();
+const todoLogic = handleTodoLogic();
+
+domManipulation.launchProjectCreator();
+domManipulation.launchTaskCreator();
+domManipulation.closeProjectCreator();
+domManipulation.closeTaskCreator();
+domManipulation.displayTask();
+// // domManipulation.openTaskDetails();
+// domManipulation.closeTaskDetails();
+
+todoLogic.createProject();
